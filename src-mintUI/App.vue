ba342e56c6e6585ab5ba782a787f6280a4c8d183
liudenghui <15564835101@163.com>
2019-08-07 12:31:19 +0800

    学习mintUI组件库

---
+++ src-mintUI/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- @click：mintUI自定义点击事件（非Vue原生事件）-->
+  <!--若使用Vue原生事件：@click.native（后需加.native）-->
+  <mt-button @click="handleClick" type="primary" style="width: 100%">Test</mt-button>
+</template>
+
+<script>
+  import {Toast} from 'mint-ui'
+  export default {
+    methods: {
+      handleClick () {
+        Toast('点击了测试');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
